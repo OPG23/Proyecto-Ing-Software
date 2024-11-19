c41996d28eaa123d596214f5e637e42a886dd37c
Nicolas Rojas <141506652+AlessioFTP@users.noreply.github.com>
2024-11-19 13:59:51 -0500

    Actualizacion cpp main
    En la segunda versión, se agrega la creación del directorio Cuentas y se ajusta la función agregarCuentasDelArchivo para aceptar el vector de productos. Se optimiza el manejo de las cuentas, leyendo y actualizando archivos como datos_carrito.csv y datos_tarjeta.csv. También se introduce un identificador id para los productos y se ajusta el carrito de compras si algún producto ya no está disponible. En general, la versión mejora la organización y el manejo de datos de usuarios y productos.

--- Proyecto/src/main.cpp
+++ Proyecto/src/main.cpp
@@ -1,88 +1,277 @@
 #include <iostream>
 
+#include <string>
+
 #include "../include/menus.h"
 
 using namespace std;
 
-void agregarCuentasDelArchivo(vector<Cuenta> &cuentas);
+void agregarCuentasDelArchivo(vector<Cuenta> &cuentas, vector<Producto> productos);
 
 void agregarProductosDelArchivo(vector<Producto> &productos);
+
 
 int main(){
 	
 	setlocale(LC_ALL, "SPANISH");
 	
+	system("mkdir ..\\Cuentas\\");
+	
 	vector<Cuenta> cuentas;
 	
 	vector<Producto> productos;
 	
-	agregarCuentasDelArchivo(cuentas);
-	
 	agregarProductosDelArchivo(productos);
-
+	
+	agregarCuentasDelArchivo(cuentas, productos);
+	
 	if(cuentas.size() > 1){
 		
 		ordenarCuentas(cuentas, 0, cuentas.size() - 1);
 	}
+	
 	
     menuPrincipal(cuentas, productos);
     
     return 0;
 }
 
-void agregarCuentasDelArchivo(vector<Cuenta> &cuentas){
+void agregarCuentasDelArchivo(vector<Cuenta> &cuentas, vector<Producto> productos){
 	
-	fstream registrosCuentas("../Cuentas/registros_cuentas.csv");
+	string carpetaProductos = "..\\Cuentas\\*";
+   	
+    WIN32_FIND_DATA informacionCarpeta;
+    
+    HANDLE carpetaEncontrada = FindFirstFile(carpetaProductos.c_str(), &informacionCarpeta);
 	
-	ifstream registrosCuentasComprobar("../Cuentas/registros_cuentas.csv");
-	
-	if(!registrosCuentasComprobar.is_open()){
+	do {
+        	
+        if (informacionCarpeta.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
+            	
+            string nombreCarpeta = informacionCarpeta.cFileName;
+                
+            if (nombreCarpeta != "." && nombreCarpeta != "..") {
+            	
+            	string nombre, correo, celular, usuario, clave, numeroTarjeta, cvvTarjeta, fechaVencimiento, nombreTitularTarjeta;
+            	
+            	bool modificarCarrito = false;
+            	
+            	vector<int> idsCarrito;
+               	
+                string rutaDatosUsuarioCuenta = "..\\Cuentas\\" + nombreCarpeta + "\\datos_cuenta.csv";
+               	
+                string rutaDatosTarjetaCuenta = "..\\Cuentas\\" + nombreCarpeta + "\\datos_tarjeta.csv";
+               	
+                string rutaDatosCarritoCuenta = "..\\Cuentas\\" + nombreCarpeta + "\\datos_carrito.csv";
+                
+                string rutaDireccionesCuenta = "..\\Cuentas\\" + nombreCarpeta + "\\datos_direccion.csv";
+                
+                string rutaProductosComprados = "..\\Cuentas\\" + nombreCarpeta + "\\datos_productos_comprados.csv";
+                
+                string rutaProductosEnVenta = "..\\Cuentas\\" + nombreCarpeta + "\\datos_productos_venta.csv";
+                
+                fstream registrosDatosUsuarioCuenta(rutaDatosUsuarioCuenta);
+                
+                fstream registrosDatosTarjetaCuenta(rutaDatosTarjetaCuenta);
+                
+                fstream registrosDatosCarritoCuenta(rutaDatosCarritoCuenta);
+                
+                fstream registrosDatosDirecionesCuenta(rutaDireccionesCuenta);
+                
+                fstream registrosDatosProductosComprados(rutaProductosComprados);
+                
+                fstream registrosDatosProductosEnVenta(rutaProductosEnVenta);
+                
+				string linea;
 		
-		ofstream crearRegistrosCuentas("../Cuentas/registros_cuentas.csv");
+				getline(registrosDatosUsuarioCuenta, linea);
 		
-		crearRegistrosCuentas << "Nombre" << ";" << "Correo" << ";" << "Celular" << ";" << "Usuario" << ";" << "Clave" << ";" << "Numero Tarjeta" << ";" << "CVV de Tarjeta" << ";" << "Fecha De Vencimiento"  << ";" << "Nombre De Tarjeta";
-	
-		crearRegistrosCuentas.close();
-	}else{
-		string linea;
+				getline(registrosDatosUsuarioCuenta, linea);
+					
+				stringstream Linea (linea);
+					
+				getline(Linea, nombre, ';');
+					
+				getline(Linea, correo, ';');
+					
+				getline(Linea, celular, ';');
+					
+				getline(Linea, usuario, ';');
+					
+				getline(Linea, clave, ';');
 		
-		getline(registrosCuentas, linea);
-		
-		while(getline(registrosCuentas, linea)){
-			
-			string nombre, correo, celular, usuario, clave, numeroTarjeta, cvvTarjeta, fechaVencimiento, nombreTitularTarjeta;
-			
-			stringstream Linea (linea);
-			
-			getline(Linea, nombre, ';');
-			
-			getline(Linea, correo, ';');
-			
-			getline(Linea, celular, ';');
-			
-			getline(Linea, usuario, ';');
-			
-			getline(Linea, clave, ';');
-			
-			getline(Linea, numeroTarjeta, ';');
-			
-			getline(Linea, cvvTarjeta, ';');
-			
-			getline(Linea, fechaVencimiento, ';');
-			
-			getline(Linea, nombreTitularTarjeta, ';');
-			
-			Cuenta cuenta;
-			
-			cuenta.agregarDatos(usuario, clave, nombre, correo, celular, numeroTarjeta, cvvTarjeta, fechaVencimiento, nombreTitularTarjeta);
-			
-			cuentas.push_back(cuenta);
-		}
-	}
+				getline(registrosDatosTarjetaCuenta, linea);
+                
+				Cuenta cuenta;
+				
+				cuenta.agregarDatos(usuario, clave, nombre, correo, celular);
+				
+				while(getline(registrosDatosTarjetaCuenta, linea)){
+							
+					string numeroTarjeta;
+					
+					string cvvTarjeta;
+					
+					string fechaVencimiento;
+					
+					string nombreTitularTarjeta;
+						
+					stringstream Linea (linea);
+				
+					getline(Linea, numeroTarjeta, ';');
+					
+					getline(Linea, cvvTarjeta, ';');
+					
+					getline(Linea, fechaVencimiento, ';');
+					
+					getline(Linea, nombreTitularTarjeta, ';');
+					
+					if(numeroTarjeta != ""){
+						
+						Tarjeta tarjeta;
+						
+						tarjeta.agregarTarjeta(numeroTarjeta, cvvTarjeta, fechaVencimiento, nombreTitularTarjeta);
+						
+						cuenta.setTarjeta(tarjeta);
+						
+					}
+					
+				}
+				
+				while(getline(registrosDatosCarritoCuenta, linea)){
+					
+					string id;
+					
+					stringstream Linea (linea);
+					
+					getline(Linea, id);
+					
+					Producto producto;
+					
+					if(buscarProductoPorID(producto, stoi(id), productos)){
+						
+						cuenta.agregarProductoCarrito(producto);
+						
+						idsCarrito.push_back(stoi(id));
+						
+					}else{
+						
+						modificarCarrito = true;
+					}
+				}
+				
+				if(modificarCarrito){
+					
+					fstream modificarCarrito(rutaDatosCarritoCuenta, ios::out);
+					
+					for(int i = 0; i < idsCarrito.size(); i++){
+						
+						modificarCarrito << idsCarrito[i] << endl;
+					}
+					
+				}
+				
+				getline(registrosDatosDirecionesCuenta, linea);
+				
+				while(getline(registrosDatosDirecionesCuenta, linea)){
+					
+					string direccionCompleta;
+					
+					string datosAdicionales;
+					
+					string puntosDeRefetencia;
+					
+					stringstream Linea (linea);
+					
+					getline(Linea, direccionCompleta, ';');
+					
+					getline(Linea, datosAdicionales, ';');
+					
+					getline(Linea, puntosDeRefetencia, ';');
+					
+					if(direccionCompleta != ""){
+						
+						Direccion direccion;
+						
+						direccion.agregarDatosDireccion(direccionCompleta, datosAdicionales, puntosDeRefetencia);
+						
+						cuenta.setDireccion(direccion);
+						
+					}
+					
+				}
+				
+				getline(registrosDatosProductosComprados, linea);
+				
+				while(getline(registrosDatosProductosComprados, linea)){
+					
+					string nombreDelProducto, valorTransaccion, medioDePago, fecha, direccionDeEntrega, idProducto, idCompra;
+					
+					stringstream Linea (linea);
+					
+					getline(Linea, nombreDelProducto, ';');
+					
+					getline(Linea, valorTransaccion, ';');
+					
+					getline(Linea, medioDePago, ';');
+					
+					getline(Linea, fecha, ';');
+					
+					getline(Linea, direccionDeEntrega, ';');
+					
+					getline(Linea, idProducto, ';');
+					
+					Producto producto;
+					
+					bool variableNoUsable = buscarProductoPorID(producto, stoi(idProducto), productos);
+					
+					Compra compra;
+					
+					compra.agregarCompra(producto, fecha, medioDePago, direccionDeEntrega);
+					
+					cuenta.agregarProductoComprado(compra);
+				}
+				
+				while(getline(registrosDatosProductosEnVenta, linea)){
+					
+					string idProducto;
+					
+					int posicionProducto = 0;
+					
+					stringstream Linea (linea);
+					
+					getline(Linea, idProducto);
+					
+					if(idProducto != ""){
+					
+						posicionProducto = conocerPosicionProductoPorID(stoi(idProducto), productos);
+					
+						cuenta.agregarProductoEnVenta(productos[posicionProducto]);
+						
+					}
+					
+				}
+                
+				
+				cuentas.push_back(cuenta);
+				
+				registrosDatosUsuarioCuenta.close();
+				
+				registrosDatosTarjetaCuenta.close();
+				
+				registrosDatosCarritoCuenta.close();
+				
+                registrosDatosDirecionesCuenta.close();
+                
+                registrosDatosProductosComprados.close();
+				
+				registrosDatosProductosEnVenta.close();
+            }
+        }
+    } while (FindNextFile(carpetaEncontrada, &informacionCarpeta) != 0);
+        
+    FindClose(carpetaEncontrada);
 	
-	registrosCuentasComprobar.close();
-	
-	registrosCuentas.close();
 }
 
 void agregarProductosDelArchivo(vector<Producto> &productos){
@@ -115,7 +304,7 @@ void agregarProductosDelArchivo(vector<Producto> &productos){
 	                    
 	                    fstream registroProducto (rutaRegistroProducto);
 	                    
-	                    string linea, nombre, descripcion, precio, stock;
+	                    string linea, nombre, descripcion, precio, stock, id;
 			
 						getline(registroProducto, linea);
 						
@@ -131,9 +320,11 @@ void agregarProductosDelArchivo(vector<Producto> &productos){
 						
 						getline(Linea, stock, ';');
 						
+						getline(Linea, id, ';');
+						
 						Producto producto;
 						
-						producto.agregarProducto(nombre, descripcion, precio, stoi(stock));
+						producto.agregarProducto(nombre, descripcion, precio, stoi(stock), id);
 						
 						productos.push_back(producto);
                 	}
